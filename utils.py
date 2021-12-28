efcc479755febedcfc1360e5fd1da67c64475254
Naiyu Yin <4025591-N.Yin@users.noreply.gitlab.com>
2021-12-28 13:33:19 -0500

    add new

---
+++ utils.py
@@ -0,0 +1,431 @@
+import numpy as np
+from scipy.special import expit as sigmoid
+import igraph as ig
+import random
+import copy
+
+
+def set_random_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+
+
+def is_dag(W):
+    G = ig.Graph.Weighted_Adjacency(W.tolist())
+    return G.is_dag()
+
+
+def simulate_dag(d, s0, graph_type):
+    """Simulate random DAG with some expected number of edges.
+
+    Args:
+        d (int): num of nodes
+        s0 (int): expected num of edges
+        graph_type (str): ER, SF, BP
+
+    Returns:
+        B (np.ndarray): [d, d] binary adj matrix of DAG
+    """
+
+    def _random_permutation(M):
+        # np.random.permutation permutes first axis only
+        P = np.random.permutation(np.eye(M.shape[0]))
+        return P.T @ M @ P
+
+    def _random_acyclic_orientation(B_und):
+        return np.tril(_random_permutation(B_und), k=-1)
+
+    def _graph_to_adjmat(G):
+        return np.array(G.get_adjacency().data)
+
+    if graph_type == 'ER':
+        # Erdos-Renyi
+        G_und = ig.Graph.Erdos_Renyi(n=d, m=s0)
+        B_und = _graph_to_adjmat(G_und)
+        B = _random_acyclic_orientation(B_und)
+    elif graph_type == 'SF':
+        # Scale-free, Barabasi-Albert
+        G = ig.Graph.Barabasi(n=d, m=int(round(s0 / d)), directed=True)
+        B = _graph_to_adjmat(G)
+    elif graph_type == 'BP':
+        # Bipartite, Sec 4.1 of (Gu, Fu, Zhou, 2018)
+        top = int(0.2 * d)
+        G = ig.Graph.Random_Bipartite(top, d - top, m=s0, directed=True, neimode=ig.OUT)
+        B = _graph_to_adjmat(G)
+    else:
+        raise ValueError('unknown graph type')
+    B_perm = _random_permutation(B)
+    assert ig.Graph.Adjacency(B_perm.tolist()).is_dag()
+    return B_perm
+
+
+def simulate_parameter(B, w_ranges=((-2.0, -0.5), (0.5, 2.0))):
+    """Simulate SEM parameters for a DAG.
+
+    Args:
+        B (np.ndarray): [d, d] binary adj matrix of DAG
+        w_ranges (tuple): disjoint weight ranges
+
+    Returns:
+        W (np.ndarray): [d, d] weighted adj matrix of DAG
+    """
+    W = np.zeros(B.shape)
+    S = np.random.randint(len(w_ranges), size=B.shape)  # which range
+    for i, (low, high) in enumerate(w_ranges):
+        U = np.random.uniform(low=low, high=high, size=B.shape)
+        W += B * (S == i) * U
+    return W
+
+
+def simulate_linear_sem(W, n, sem_type, noise_scale=None):
+    """Simulate samples from linear SEM with specified type of noise.
+
+    For uniform, noise z ~ uniform(-a, a), where a = noise_scale.
+
+    Args:
+        W (np.ndarray): [d, d] weighted adj matrix of DAG
+        n (int): num of samples, n=inf mimics population risk
+        sem_type (str): gauss, exp, gumbel, uniform, logistic, poisson
+        noise_scale (np.ndarray): scale parameter of additive noise, default all ones
+
+    Returns:
+        X (np.ndarray): [n, d] sample matrix, [d, d] if n=inf
+    """
+
+    def _simulate_single_equation(X, w, scale):
+        """X: [n, num of parents], w: [num of parents], x: [n]"""
+        if sem_type == 'gauss':
+            z = np.random.normal(scale=scale, size=n)
+            x = X @ w + z
+        elif sem_type == 'exp':
+            z = np.random.exponential(scale=scale, size=n)
+            x = X @ w + z
+        elif sem_type == 'gumbel':
+            z = np.random.gumbel(scale=scale, size=n)
+            x = X @ w + z
+        elif sem_type == 'uniform':
+            z = np.random.uniform(low=-scale, high=scale, size=n)
+            x = X @ w + z
+        elif sem_type == 'logistic':
+            x = np.random.binomial(1, sigmoid(X @ w)) * 1.0
+        elif sem_type == 'poisson':
+            x = np.random.poisson(np.exp(X @ w)) * 1.0
+        else:
+            raise ValueError('unknown sem type')
+        return x
+
+    d = W.shape[0]
+    if noise_scale is None:
+        scale_vec = np.ones(d)
+    elif np.isscalar(noise_scale):
+        scale_vec = noise_scale * np.ones(d)
+    else:
+        if len(noise_scale) != d:
+            raise ValueError('noise scale must be a scalar or has length d')
+        scale_vec = noise_scale
+    if not is_dag(W):
+        raise ValueError('W must be a DAG')
+    if np.isinf(n):  # population risk for linear gauss SEM
+        if sem_type == 'gauss':
+            # make 1/d X'X = true cov
+            X = np.sqrt(d) * np.diag(scale_vec) @ np.linalg.inv(np.eye(d) - W)
+            return X
+        else:
+            raise ValueError('population risk not available')
+    # empirical risk
+    G = ig.Graph.Weighted_Adjacency(W.tolist())
+    ordered_vertices = G.topological_sorting()
+    assert len(ordered_vertices) == d
+    X = np.zeros([n, d])
+    for j in ordered_vertices:
+        parents = G.neighbors(j, mode=ig.IN)
+        X[:, j] = _simulate_single_equation(X[:, parents], W[parents, j], scale_vec[j])
+    return X
+
+
+def simulate_linear_neq(W, W_0, B, B_0, n):
+    def _simulate_single_equation(X, w, w0, b, b0):
+        """X: [n, num of parents], w: [num of parents], x: [n]"""
+        X_n = (X - np.mean(X, axis=0)) / np.std(X, axis=0)  # normalize X
+        x = np.random.normal(loc=X @ w + w0, scale=np.sqrt(np.exp(X_n @ b + b0)), size=(n,))
+        return x
+
+    d = W.shape[0]
+    if not is_dag(W):
+        raise ValueError('W must be a DAG')
+    G = ig.Graph.Weighted_Adjacency(W.tolist())
+    ordered_vertices = G.topological_sorting()
+    assert len(ordered_vertices) == d
+    X = np.zeros([n, d])
+    for j in ordered_vertices:
+        parents = G.neighbors(j, mode=ig.IN)
+        X[:, j] = _simulate_single_equation(X[:, parents], W[parents, j], W_0[j], B[parents, j], B_0[j])
+    return X
+
+
+def simulate_linear_neq_v2(W, W_0, A, B, n):
+    def _simulate_single_equation(X, w, w0, a, b):
+        """X: [n, num of parents], w: [num of parents], x: [n]"""
+        var = np.random.uniform(a, b, size=(n,))
+        e = np.random.normal(loc=0, scale=np.sqrt(var), size=(n,))
+        x = X @ w + w0 + e
+        return x, var
+
+    d = W.shape[0]
+    if not is_dag(W):
+        raise ValueError('W must be a DAG')
+    G = ig.Graph.Weighted_Adjacency(W.tolist())
+    ordered_vertices = G.topological_sorting()
+    assert len(ordered_vertices) == d
+    assert len(A) == d
+    assert len(B) == d
+    X = np.zeros([n, d])
+    Var = np.zeros([n, d])
+    for j in ordered_vertices:
+        parents = G.neighbors(j, mode=ig.IN)
+        X[:, j], Var[:,  j] = _simulate_single_equation(X[:, parents], W[parents, j], W_0[j], A[j], B[j])
+    return X,  Var
+
+
+def simulate_linear_eq_bias(W, W_0, n):
+    def _simulate_single_equation(X, w, w0):
+        """X: [n, num of parents], w: [num of parents], x: [n]"""
+        z = np.random.normal(loc=0, scale=1, size=(n,))
+        x = X @ w + w0 + z
+        return x
+
+    d = W.shape[0]
+    if not is_dag(W):
+        raise ValueError('W must be a DAG')
+    G = ig.Graph.Weighted_Adjacency(W.tolist())
+    ordered_vertices = G.topological_sorting()
+    assert len(ordered_vertices) == d
+    X = np.zeros([n, d])
+    for j in ordered_vertices:
+        parents = G.neighbors(j, mode=ig.IN)
+        X[:, j] = _simulate_single_equation(X[:, parents], W[parents, j], W_0[j])
+    return X
+
+
+def simulate_nonlinear_sem(B, n, sem_type, noise_scale=None):
+    """Simulate samples from nonlinear SEM.
+
+    Args:
+        B (np.ndarray): [d, d] binary adj matrix of DAG
+        n (int): num of samples
+        sem_type (str): mlp, mim, gp, gp-add
+        noise_scale (np.ndarray): scale parameter of additive noise, default all ones
+
+    Returns:
+        X (np.ndarray): [n, d] sample matrix
+    """
+
+    def _simulate_single_equation(X, scale):
+        """X: [n, num of parents], x: [n]"""
+        z = np.random.normal(scale=scale, size=n)
+        pa_size = X.shape[1]
+        if pa_size == 0:
+            return z
+        if sem_type == 'mlp':
+            hidden = 100
+            W1 = np.random.uniform(low=0.5, high=2.0, size=[pa_size, hidden])
+            W1[np.random.rand(*W1.shape) < 0.5] *= -1
+            W2 = np.random.uniform(low=0.5, high=2.0, size=hidden)
+            W2[np.random.rand(hidden) < 0.5] *= -1
+            x = sigmoid(X @ W1) @ W2 + z
+        elif sem_type == 'mim':
+            w1 = np.random.uniform(low=0.5, high=2.0, size=pa_size)
+            w1[np.random.rand(pa_size) < 0.5] *= -1
+            w2 = np.random.uniform(low=0.5, high=2.0, size=pa_size)
+            w2[np.random.rand(pa_size) < 0.5] *= -1
+            w3 = np.random.uniform(low=0.5, high=2.0, size=pa_size)
+            w3[np.random.rand(pa_size) < 0.5] *= -1
+            x = np.tanh(X @ w1) + np.cos(X @ w2) + np.sin(X @ w3) + z
+        elif sem_type == 'gp':
+            from sklearn.gaussian_process import GaussianProcessRegressor
+            gp = GaussianProcessRegressor()
+            x = gp.sample_y(X, random_state=None).flatten() + z
+        elif sem_type == 'gp-add':
+            from sklearn.gaussian_process import GaussianProcessRegressor
+            gp = GaussianProcessRegressor()
+            x = sum([gp.sample_y(X[:, i, None], random_state=None).flatten()
+                     for i in range(X.shape[1])]) + z
+        else:
+            raise ValueError('unknown sem type')
+        return x
+
+    d = B.shape[0]
+    scale_vec = noise_scale if noise_scale else np.ones(d)
+    X = np.zeros([n, d])
+    G = ig.Graph.Adjacency(B.tolist())
+    ordered_vertices = G.topological_sorting()
+    assert len(ordered_vertices) == d
+    for j in ordered_vertices:
+        parents = G.neighbors(j, mode=ig.IN)
+        X[:, j] = _simulate_single_equation(X[:, parents], scale_vec[j])
+    return X
+
+
+def count_accuracy(B_true, B_est):
+    """Compute various accuracy metrics for B_est.
+
+    true positive = predicted association exists in condition in correct direction
+    reverse = predicted association exists in condition in opposite direction
+    false positive = predicted association does not exist in condition
+
+    Args:
+        B_true (np.ndarray): [d, d] ground truth graph, {0, 1}
+        B_est (np.ndarray): [d, d] estimate, {0, 1, -1}, -1 is undirected edge in CPDAG
+
+    Returns:
+        fdr: (reverse + false positive) / prediction positive
+        tpr: (true positive) / condition positive
+        fpr: (reverse + false positive) / condition negative
+        shd: undirected extra + undirected missing + reverse
+        nnz: prediction positive
+    """
+    if (B_est == -1).any():  # cpdag
+        if not ((B_est == 0) | (B_est == 1) | (B_est == -1)).all():
+            raise ValueError('B_est should take value in {0,1,-1}')
+        if ((B_est == -1) & (B_est.T == -1)).any():
+            raise ValueError('undirected edge should only appear once')
+    else:  # dag
+        if not ((B_est == 0) | (B_est == 1)).all():
+            raise ValueError('B_est should take value in {0,1}')
+        if not is_dag(B_est):
+            raise ValueError('B_est should be a DAG')
+    d = B_true.shape[0]
+    # linear index of nonzeros
+    pred_und = np.flatnonzero(B_est == -1)
+    pred = np.flatnonzero(B_est == 1)
+    cond = np.flatnonzero(B_true)
+    cond_reversed = np.flatnonzero(B_true.T)
+    cond_skeleton = np.concatenate([cond, cond_reversed])
+    # true pos
+    true_pos = np.intersect1d(pred, cond, assume_unique=True)
+    # treat undirected edge favorably
+    true_pos_und = np.intersect1d(pred_und, cond_skeleton, assume_unique=True)
+    true_pos = np.concatenate([true_pos, true_pos_und])
+    # false pos
+    false_pos = np.setdiff1d(pred, cond_skeleton, assume_unique=True)
+    false_pos_und = np.setdiff1d(pred_und, cond_skeleton, assume_unique=True)
+    false_pos = np.concatenate([false_pos, false_pos_und])
+    # reverse
+    extra = np.setdiff1d(pred, cond, assume_unique=True)
+    reverse = np.intersect1d(extra, cond_reversed, assume_unique=True)
+    # compute ratio
+    pred_size = len(pred) + len(pred_und)
+    cond_neg_size = 0.5 * d * (d - 1) - len(cond)
+    fdr = float(len(reverse) + len(false_pos)) / max(pred_size, 1)
+    tpr = float(len(true_pos)) / max(len(cond), 1)
+    fpr = float(len(reverse) + len(false_pos)) / max(cond_neg_size, 1)
+    # structural hamming distance
+    pred_lower = np.flatnonzero(np.tril(B_est + B_est.T))
+    cond_lower = np.flatnonzero(np.tril(B_true + B_true.T))
+    extra_lower = np.setdiff1d(pred_lower, cond_lower, assume_unique=True)
+    missing_lower = np.setdiff1d(cond_lower, pred_lower, assume_unique=True)
+    shd = len(extra_lower) + len(missing_lower) + len(reverse)
+    # return {'fdr': fdr, 'tpr': tpr, 'fpr': fpr, 'shd': shd, 'nnz': pred_size}
+    # return {'shd': shd, 'extra': len(extra_lower), 'missing': len(missing_lower), 'reverse': len(reverse), 'nnz': pred_size}
+    return shd, len(extra_lower), len(missing_lower), len(reverse)
+
+
+def shd_curve(A_gt, A, thres, post=False):
+    SHDs = np.zeros([len(thres), ])
+    # uc = np.zeros([len(thres), ])
+    for iter in range(len(thres)):
+        G = copy.deepcopy(A)
+        G[np.abs(G) < thres[iter]] = 0
+        if post == True:
+            G, _ = threshold_till_dag(G)
+        G = (G != 0).astype("int")
+        # uc[iter] = np.sum(G * variances) / np.sum(G)
+        SHDs[iter], _, _, _ = count_accuracy(A_gt, G)
+    AUC = (thres[1] - thres[0]) * (np.sum(SHDs) - SHDs[-1])
+    return SHDs, AUC
+
+
+def PrecisionRecall_curve(A_gt, A):
+    thres = sorted(np.unique(np.abs(A)))
+    precision = np.zeros([len(thres), ])
+    recall = np.zeros([len(thres), ])
+    d = A.shape[0]
+    A_gt = (A_gt != 0).astype("int")
+    for iter in range(len(thres)):
+        G = copy.deepcopy(A)
+        G[np.abs(G) < thres[iter]] = 0
+        G = (G != 0).astype("int")
+        tp = 0
+        fp = 0
+        fn = 0
+        for i in range(d):
+            for j in range(d):
+                if A_gt[i, j] == 1 and G[i, j] == 1:
+                    tp += 1
+                elif A_gt[i, j] == 1 and G[i, j] == 0:
+                    fn += 1
+                elif A_gt[i, j] == 0 and G[i, j] == 1:
+                    fp += 1
+        # print(tp)
+        # print(fn)
+        # print(fp)
+        precision[iter] = tp / (tp + fp)
+        recall[iter] = tp / (tp + fn)
+        from sklearn import metrics
+        AUC = metrics.auc(recall, precision)
+    return precision, recall, AUC
+
+
+def threshold_till_dag(B):
+    """Remove the edges with smallest absolute weight until a DAG is obtained.
+
+    Args:
+        B (numpy.ndarray): [d, d] weighted matrix.
+
+    Returns:
+        numpy.ndarray: [d, d] weighted matrix of DAG.
+        float: Minimum threshold to obtain DAG.
+    """
+    if is_dag(B):
+        return B, 0
+
+    B = np.copy(B)
+    # Get the indices with non-zero weight
+    nonzero_indices = np.where(B != 0)
+    # Each element in the list is a tuple (weight, j, i)
+    weight_indices_ls = list(zip(B[nonzero_indices],
+                                 nonzero_indices[0],
+                                 nonzero_indices[1]))
+    # Sort based on absolute weight
+    sorted_weight_indices_ls = sorted(weight_indices_ls, key=lambda tup: abs(tup[0]))
+
+    for weight, j, i in sorted_weight_indices_ls:
+        if is_dag(B):
+            # A DAG is found
+            break
+
+        # Remove edge with smallest absolute weight
+        B[j, i] = 0
+        dag_thres = abs(weight)
+
+    return B, dag_thres
+
+
+def postprocess(B, graph_thres=0.3):
+    """Post-process estimated solution:
+        (1) Thresholding.
+        (2) Remove the edges with smallest absolute weight until a DAG
+            is obtained.
+
+    Args:
+        B (numpy.ndarray): [d, d] weighted matrix.
+        graph_thres (float): Threshold for weighted matrix. Default: 0.3.
+
+    Returns:
+        numpy.ndarray: [d, d] weighted matrix of DAG.
+    """
+    B = np.copy(B)
+    B[np.abs(B) <= graph_thres] = 0  # Thresholding
+    B, _ = threshold_till_dag(B)
+
+    return B
